367e15a909c1cff38d213b642ca0e9c749310c40
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-02-05 21:53:15 -0500

    setAttribute src for forecast icons
    pulled icon element from API call

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ function placeIcon(iconId) {
 }
 
 // function forecastIcon(iconId2) {
-//   let foreIcon = 
+//   let foreIcon =
 // }
 
 /////
@@ -97,7 +97,6 @@ function placeIcon(iconId) {
 ///
 // 7 day forecast
 
-  
 function displayForecast(response) {
   document.querySelector(".sun-hi").innerHTML = Math.round(
     response.data.data[0].app_max_temp
@@ -141,8 +140,58 @@ function displayForecast(response) {
   document.querySelector(".sat-lo").innerHTML = Math.round(
     response.data.data[6].app_min_temp
   );
-  
-  console.log(response);
+
+  ///
+  // forecast icons
+
+  let sunIcon = document.querySelector("#sun-icon");
+  sunIcon.setAttribute(
+    "src",
+    `https://www.weatherbit.io/static/img/icons/${response.data.data[0].weather.icon}.png`
+  );
+  sunIcon.setAttribute("alt", response.data.data[0].weather.description);
+
+  let monIcon = document.querySelector("#mon-icon");
+  monIcon.setAttribute(
+    "src",
+    `https://www.weatherbit.io/static/img/icons/${response.data.data[1].weather.icon}.png`
+  );
+  monIcon.setAttribute("alt", response.data.data[1].weather.description);
+
+  let tuesIcon = document.querySelector("#tues-icon");
+  tuesIcon.setAttribute(
+    "src",
+    `https://www.weatherbit.io/static/img/icons/${response.data.data[2].weather.icon}.png`
+  );
+  tuesIcon.setAttribute("alt", response.data.data[2].weather.description);
+
+  let wedIcon = document.querySelector("#wed-icon");
+  wedIcon.setAttribute(
+    "src",
+    `https://www.weatherbit.io/static/img/icons/${response.data.data[3].weather.icon}.png`
+  );
+  wedIcon.setAttribute("alt", response.data.data[3].weather.description);
+
+  let thursIcon = document.querySelector("#thurs-icon");
+  thursIcon.setAttribute(
+    "src",
+    `https://www.weatherbit.io/static/img/icons/${response.data.data[4].weather.icon}.png`
+  );
+  thursIcon.setAttribute("alt", response.data.data[4].weather.description);
+
+  let friIcon = document.querySelector("#fri-icon");
+  friIcon.setAttribute(
+    "src",
+    `https://www.weatherbit.io/static/img/icons/${response.data.data[5].weather.icon}.png`
+  );
+  friIcon.setAttribute("alt", response.data.data[5].weather.description);
+
+  let satIcon = document.querySelector("#sat-icon");
+  satIcon.setAttribute(
+    "src",
+    `https://www.weatherbit.io/static/img/icons/${response.data.data[6].weather.icon}.png`
+  );
+  satIcon.setAttribute("alt", response.data.data[6].weather.description);
 }
 
 /////
@@ -156,8 +205,8 @@ function searchEngine(city) {
 
   axios.get(apiUrl).then(displayTemp);
 
-  let forecastKey = "13ce2da2cd2d4da6b6a30677bd0ecc0d"
-  let forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${forecastKey}&units=I`
+  let forecastKey = "13ce2da2cd2d4da6b6a30677bd0ecc0d";
+  let forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${forecastKey}&units=I`;
 
   axios.get(forecastUrl).then(displayForecast);
 }
@@ -194,26 +243,22 @@ function convertCelsius(event) {
   cels.classList.add("active");
 }
 
-
 function showPositon(position) {
   let apiKey = "16fb7fe8628dfdd6476ce112c8b8470c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(displayTemp);
 
-  let forecastKey = "13ce2da2cd2d4da6b6a30677bd0ecc0d"
-  let forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${position.coords.latitude}&lon=${position.coords.longitude}&key=${forecastKey}&units=I`
+  let forecastKey = "13ce2da2cd2d4da6b6a30677bd0ecc0d";
+  let forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${position.coords.latitude}&lon=${position.coords.longitude}&key=${forecastKey}&units=I`;
 
   axios.get(forecastUrl).then(displayForecast);
-
 }
 
 function getLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPositon);
 }
-
-
 
 /////
 ////
